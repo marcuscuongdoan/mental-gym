c1eb447141ddceb008a74f31534290da38cc1732
Cuong Doan <marcus.cuong.doan@gmail.com>
2021-12-21 03:17:10 +0700

    fix language switcher

--- src/layouts/languageSwitcher.jsx
+++ src/layouts/languageSwitcher.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import lstrings from "../language";
+import { spring, AnimatedSwitch } from "react-router-transition";
 
 function LanguageSwitcher({ children }) {
   const [lang, setLang] = useState({ language: "en" });
@@ -9,6 +10,41 @@ function LanguageSwitcher({ children }) {
     lstrings.setLanguage(otherLang);
     setLang({ ...lang, language: otherLang });
   }
+
+  function mapStyles(styles) {
+    return {
+      opacity: styles.opacity,
+      transform: `scale(${styles.scale})`,
+    };
+  }
+
+  // wrap the `spring` helper to use a bouncy config
+  function bounce(val) {
+    return spring(val, {
+      stiffness: 330,
+      damping: 22,
+    });
+  }
+
+  // child matches will...
+  const bounceTransition = {
+    // start in a transparent, upscaled state
+    atEnter: {
+      opacity: 0,
+      scale: 0.8,
+    },
+    // leave in a transparent, downscaled state
+    atLeave: {
+      opacity: bounce(0),
+      scale: bounce(2),
+    },
+    // and rest at an opaque, normally-scaled state
+    atActive: {
+      opacity: bounce(1),
+      scale: bounce(1),
+    },
+  };
+
   return (
     <>
       <div
@@ -29,7 +65,17 @@ function LanguageSwitcher({ children }) {
         </span>
         {lstrings.language}
       </div>
-      {children}
+
+      <AnimatedSwitch
+        atEnter={bounceTransition.atEnter}
+        atLeave={bounceTransition.atLeave}
+        atActive={bounceTransition.atActive}
+        mapStyles={mapStyles}
+        className="route-wrapper"
+        key={lstrings.getLanguage()}
+      >
+        {children}
+      </AnimatedSwitch>
     </>
   );
 }
