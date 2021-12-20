c0c2cbf229fccc20d7e7b9d6c1b5f98862b3afcb
Cuong Doan <marcus.cuong.doan@gmail.com>
2021-12-20 14:16:07 +0700

    remove unused import

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,6 @@
 function Modal({ show, handleClose, children }) {
   const showHideClassName = show ? "Modal" : "Modal hide";
 
-  const handleOutsideClick = (e) => {
-    console.log(e);
-    show = false;
-    console.log(showHideClassName);
-    // if (!e.contains(e.target)) this.handleClose();
-  };
-
   return (
     <div className={showHideClassName} onClick={handleClose}>
       <div className="content">{children}</div>
