c35e95edae6226d99561931c158902cf6e5b8809
Cuong Doan <marcus.cuong.doan@gmail.com>
2021-12-21 04:02:16 +0700

    update import

--- src/language.js
+++ src/language.js
@@ -15,6 +15,63 @@ const lstrings = new LocalizedStrings({
     write: "Write",
     yoga: "Yoga",
     note: "*Please using web browser like Google Chrome or Mozilla Firefox for best experience",
+    theatreRoom: {
+      wakeup: {
+        title: "Wake Up",
+        left: "Early",
+        right: "Late",
+      },
+      shower: {
+        title: "Take shower",
+        left: "Take",
+        right: "Nooooo",
+      },
+      tidy: {
+        title: "Are you tidy",
+        left: "Tidy",
+        right: "Whatever",
+      },
+      drink: {
+        title: "Have some water",
+        left: "Drinkkkk",
+        right: "Nooooo",
+      },
+      breakfast: {
+        title: "Do you have breakfast",
+        left: "Eat",
+        right: "Nooooo",
+      },
+      gym: {
+        title: "Do you go to gym",
+        left: "Yesssss",
+        right: "Nooooo",
+      },
+      read: {
+        title: "Will you read",
+        left: "Of course!",
+        right: "What is book?",
+      },
+      journal: {
+        title: "How about journal",
+        left: "Write",
+        right: "Skip",
+      },
+      study: {
+        title: "Do you study",
+        left: "Yessss",
+        right: "Not matter",
+      },
+      family: {
+        title: "Take time with family",
+        left: "Yessss",
+        right: "Skip",
+      },
+      conclusion: {
+        more: "We could try more!",
+        balance: "You seem balancing!",
+        less: "Are you alright? You should try other rooms!",
+      },
+    },
   },
   vn: {
     language: "Change to English",
@@ -30,6 +87,63 @@ const lstrings = new LocalizedStrings({
     write: "Phòng Viết",
     yoga: "Phòng Yoga",
     note: "*Bạn nên sử dụng trình duyệt Google Chrome hoặc Mozilla Firefox để thưởng thức tốt nhất",
+    theatreRoom: {
+      wakeup: {
+        title: "Bạn dậy sớm hay trễ",
+        left: "Sớm",
+        right: "Trễ",
+      },
+      shower: {
+        title: "Bạn có tắm",
+        left: "Có",
+        right: "Không",
+      },
+      tidy: {
+        title: "Bạn có ngăn nắp",
+        left: "Có",
+        right: "Sao cũng được",
+      },
+      drink: {
+        title: "Uống tí nước nhé",
+        left: "Uốngggg",
+        right: "Khôngggg",
+      },
+      breakfast: {
+        title: "Bạn có ăn sáng",
+        left: "Ăn",
+        right: "Khôngggg",
+      },
+      gym: {
+        title: "Bạn có tập gym",
+        left: "Cóooo",
+        right: "Khôngggg",
+      },
+      read: {
+        title: "Bạn sẽ đọc sách chứ",
+        left: "Tất nhiên!",
+        right: "Sách là gì?",
+      },
+      journal: {
+        title: "Nhật ký thì sao",
+        left: "Có viết",
+        right: "Bỏ qua",
+      },
+      study: {
+        title: "Bạn có học không",
+        left: "Cóooo",
+        right: "Không quan trọng",
+      },
+      family: {
+        title: "Bạn có dành thời gian cho gia đình",
+        left: "Cóooo",
+        right: "Bỏ qua",
+      },
+      conclusion: {
+        more: "Chúng ta hãy cố thêm tí nữa nhé!",
+        balance: "Bạn có vẻ cân bằng đó!",
+        less: "Bạn có ổn không? Các phòng khác có thể sẽ tốt cho bạn!",
+      },
+    },
   },
 });
 
